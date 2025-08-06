e980974c662c35258a385aef7308616044c0123e
Gautampatel-28 <gautampatel7233@gmail.com>
2025-08-06 10:44:14 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Topbar from "./Components/topbar/Topbar";
 import Sidebar from "./Components/sidebar/Sidebar";
-import "./App.css";
 import Home from "./Pages/Home";
+import UserList from "./Pages/userList/UserList";
+import User from "./Pages/user/User"
 
 function App() {
   return (
     <>
-      <div>
+      <Router>
         <Topbar />
         <div className="container">
           <Sidebar />
-          {/* <div className="others">other pages</div> */}
-          <Home />
+          <Routes>  
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<UserList />} />
+            <Route path="/user/:userId" element={<User />} />
+          </Routes>
         </div>
-      </div>
+      </Router>
     </>
   );
 }
